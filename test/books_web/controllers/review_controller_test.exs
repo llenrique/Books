4442d1f46d1087f6d026b5df9f3c2fe3bea58dbf
Enrique López <enrique@MacBook-Pro-de-Enrique.local>
2019-07-21 12:19:14 -0500

    Test de algunas cosas más de los reviews

---
+++ test/books_web/controllers/review_controller_test.exs
@@ -0,0 +1,32 @@
+defmodule BooksWeb.ReviewControllerTest do
+    use BooksWeb.ConnCase
+
+    import Books.Factory
+
+    @review_test %{
+        "review" => %{"content" => "Buen codigo"}
+    }
+
+    @invalid_review_test %{
+        "review" => %{"content" => ""}
+    }
+    
+    test "create/2 Agrega un review", %{conn: conn} do
+        book = insert(:book)
+        review_params = Map.put(@review_test, "book_id", book.id)
+
+        conn = post(conn, Routes.review_path(conn, :create, book.id, review_params))
+
+        assert redirected_to(conn) =~ "/show/#{book.id}"
+    end
+    
+    test "create/2 Falla al agregar un review", %{conn: conn} do
+        book = insert(:book)
+
+        review_params = Map.put(@invalid_review_test, "book_id", book.id)
+
+        conn = post(conn, Routes.review_path(conn, :create, book.id, review_params))
+
+        assert get_flash(conn, :error) == "No se pudo agregar el review"
+    end
+end
---
+++ test/helpers/review_helper_test.exs
@@ -0,0 +1,34 @@
+defmodule Books.Helpers.ReviewHelperTest do
+    use Books.DataCase
+
+    alias Books.Helpers.ReviewHelper
+    alias Books.Helpers.BookHelper
+
+    import Books.Factory
+
+    @valid_review_params %{
+        "content" => "Este es un review"
+    }
+
+    @invalid_review_params %{
+        "content" => :"12345"
+    }
+
+    test "add_review/1 Agrega un review a un libro" do
+        inserted_book = insert(:book)
+        params = %{"book_id" => inserted_book.id, "review" => @valid_review_params}
+        ReviewHelper.add_review(params)
+        %{reviews: [review]} = BookHelper.get_book(inserted_book.id)
+
+        assert review.content == "Este es un review"
+
+    end
+    
+    test "add_review/1 Falla al agregar un review a un libro" do
+        inserted_book = insert(:book)
+        params = %{"book_id" => inserted_book.id, "review" => @invalid_review_params}
+        result = ReviewHelper.add_review(params)
+
+        assert result == :error
+    end
+end
